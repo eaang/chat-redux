ab555f93479f7c50cc3a99b2b201d8d6a4e1f260
Evan Ang <alethiologist@gmail.com>
2020-07-26 22:41:13 +0800

    restarting this stupid project

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
+import messagesReducer from './reducers/messages_reducer';
+import selectedChannelReducer from './reducers/selected_channel';
 import '../assets/stylesheets/application.scss';
 
 // State and reducers
+const initialState = {
+  messages: [],
+  channels: ['general', 'react', 'singapore'],
+  currentUser: 'Jimmy',
+  selectedChannel: 'general'
+};
+
+const identityReducer = (state = null) => state;
+
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  currentUser: identityReducer,
+  messages: messagesReducer,
+  channels: identityReducer,
+  selectedChannel: selectedChannelReducer
 });
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
